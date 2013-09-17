57985f62c0e214028a30f5d8e10ed39f5889cc18
Tony Salamanca <ikibits@gmail.com>
2013-09-16 23:05:58 -0500

    Se actualizo esquema de base de datos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130913074454) do
+ActiveRecord::Schema.define(version: 20130916200136) do
 
   create_table "roles", force: true do |t|
     t.string   "name"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20130913074454) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
+    t.string   "codigo"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
