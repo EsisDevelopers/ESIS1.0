70faa8ad5bf3d3c06396be476ee5bb5bf0fc6474
Tony Salamanca <ikibits@gmail.com>
2013-09-21 23:25:42 -0500

    Se agrego pluralizaciones de seccion y session

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -14,3 +14,8 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
 # end
+
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+    inflect.irregular 'seccion', 'secciones'
+    inflect.irregular 'sesion','sesiones'
+end
