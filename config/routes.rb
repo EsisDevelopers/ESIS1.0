d2429ee74d8e3970d339435ff2165ffd63efc461
Tony Salamanca <ikibits@gmail.com>
2013-09-23 23:44:06 -0500

    Se agrego modulos de admin, academico y metodos estudiante,docente y administrativo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 ESIS10::Application.routes.draw do
+  
+    
+  #get "sesiones/index"
+  namespace :admins do
+	get "dashboard/index"
+  end
+  namespace :academico do
+	resources :secciones
+  end
+  
+  
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
-  resources :users
+  #devise_for :admins, :controllers => {:sessions => "admin/sesiones" }
+  resources :users do
+	member do
+		post :estudiante
+		post :docente
+		post :administrativo
+	end
+end
 end
