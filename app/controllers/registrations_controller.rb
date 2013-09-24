45326e8f0aa015bdb95f5ee261644f88d27590d6
Tony Salamanca <ikibits@gmail.com>
2013-09-23 23:28:25 -0500

    Se agrego protegio protected

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
+  
   before_filter :update_sanitized_params, if: :devise_controller?
-
+  
+  protected
+  
   def update_sanitized_params
     devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:codigo, :email, :password, :password_confirmation)}
     devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:codigo, :email, :password, :password_confirmation, :current_password)}
   end
-
 end
